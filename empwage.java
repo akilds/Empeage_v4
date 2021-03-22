4b9498ae088e68f3e0a1d23fb20d50cb2f34c1cd
akilds <akilrainadesigan@gmail.com>
2021-03-22 10:43:15 +0530

    Employee Attendance

--- empwage.java
+++ empwage.java
@@ -1,7 +1,21 @@
-public class empwage
+import java.util.*;
+
+public class pg1
 {
  public static void main(String[] args)
  {
-  System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+  Random r = new Random();
+  int a = r.nextInt(2);
+  System.out.println(a);
+  if(a==0)
+  {
+   System.out.println("Employee Present");
+  }
+  else
+  {
+   System.out.println("Employee Absent");
+  }
  }
 }
+
+
