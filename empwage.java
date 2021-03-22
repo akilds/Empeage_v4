4e7f9f4dba2aba1b9f9e1dabfdaf2e0a3c028af6
akilds <akilrainadesigan@gmail.com>
2021-03-22 10:58:57 +0530

    Daily Wages

--- empwage.java
+++ empwage.java
@@ -1,20 +1,26 @@
 import java.util.*;
 
-public class pg1
+public class empwage
 {
  public static void main(String[] args)
  {
+  int Total_Hrs_Per_Day = 8;
+  int Wage_Per_Hr = 20;
+  int Total_Wage;
   Random r = new Random();
   int a = r.nextInt(2);
   System.out.println(a);
   if(a==0)
   {
    System.out.println("Employee Present");
+   Total_Wage = Total_Hrs_Per_Day * Wage_Per_Hr;
   }
   else
   {
    System.out.println("Employee Absent");
+   Total_Wage = 0;
   }
+  System.out.println("Employee Wage : " + Total_Wage);
  }
 }
 
