1a16c555e94889cd5ffba6eb87fe5614ae976d9a
akilds <akilrainadesigan@gmail.com>
2021-03-22 12:07:41 +0530

    Calculate till condition

--- empwage.java
+++ empwage.java
@@ -10,19 +10,24 @@ public class empwage
   int Total_Wage ;
   int Working_Days_Per_Month = 20;
   int Monthly_Wage = 0;
+  int Total_Hrs = 0;
+  int Total_Days = 0;
   Random r = new Random();
   int a = r.nextInt(3);
   System.out.println(a);
-  for(int i=1;i<=Working_Days_Per_Month;i++)
+  while(Total_Days!=20 || Total_Hrs!=100)
   {
    switch(a) 
    {
     case 0 : System.out.println("Employee Present Full time");
              Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
-             break;
+             Total_Hrs += Total_Fulltime_Hrs;
+             Total_Days++;
+				 break;
     case 1 : System.out.println("Employee Present Part time");
              Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
-             break;
+             Total_Hrs += Total_Fulltime_Hrs;
+				 break;
     case 2 : System.out.println("Employee Absent");
              Total_Wage = 0;
              break;
