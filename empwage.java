4816a7d44bf0ca243d12da504d285abbb6a44432
akilds <akilrainadesigan@gmail.com>
2021-03-22 11:12:32 +0530

    Parttime Wages

--- empwage.java
+++ empwage.java
@@ -4,16 +4,22 @@ public class empwage
 {
  public static void main(String[] args)
  {
-  int Total_Hrs_Per_Day = 8;
+  int Total_Fulltime_Hrs = 8;
+  int Total_Parttime_Hrs = 4;
   int Wage_Per_Hr = 20;
   int Total_Wage;
   Random r = new Random();
-  int a = r.nextInt(2);
+  int a = r.nextInt(3);
   System.out.println(a);
   if(a==0)
   {
-   System.out.println("Employee Present");
-   Total_Wage = Total_Hrs_Per_Day * Wage_Per_Hr;
+   System.out.println("Employee Present Full time");
+   Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
+  }
+  else if(a==1)
+  {
+   System.out.println("Employee Present Part time");
+   Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
   }
   else
   {
