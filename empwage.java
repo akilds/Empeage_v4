c5ea563d3451b53bf3950575adf4ccfb7a5d2c7c
akilds <akilrainadesigan@gmail.com>
2021-03-22 11:35:25 +0530

    Switch case

--- empwage.java
+++ empwage.java
@@ -7,24 +7,21 @@ public class empwage
   int Total_Fulltime_Hrs = 8;
   int Total_Parttime_Hrs = 4;
   int Wage_Per_Hr = 20;
-  int Total_Wage;
+  int Total_Wage ;
   Random r = new Random();
   int a = r.nextInt(3);
   System.out.println(a);
-  if(a==0)
+  switch(a) 
   {
-   System.out.println("Employee Present Full time");
-   Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
-  }
-  else if(a==1)
-  {
-   System.out.println("Employee Present Part time");
-   Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
-  }
-  else
-  {
-   System.out.println("Employee Absent");
-   Total_Wage = 0;
+   case 0 : System.out.println("Employee Present Full time");
+            Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
+            break;
+   case 1 : System.out.println("Employee Present Part time");
+            Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
+            break;
+   case 2 : System.out.println("Employee Absent");
+            Total_Wage = 0;
+            break;
   }
   System.out.println("Employee Wage : " + Total_Wage);
  }
