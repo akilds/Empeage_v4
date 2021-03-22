6b6ed1dc5990a030787232725c8d5ee625cb5f3f
akilds <akilrainadesigan@gmail.com>
2021-03-22 16:32:41 +0530

    Monthly Wage

--- empwage.java
+++ empwage.java
@@ -7,14 +7,13 @@ public class empwage
   int Total_Fulltime_Hrs = 8;
   int Total_Parttime_Hrs = 4;
   int Wage_Per_Hr = 20;
-  int Total_Wage ;
+  int Total_Wage = 0;
   int Working_Days_Per_Month = 20;
   int Monthly_Wage = 0;
   Random r = new Random();
-  int a = r.nextInt(3);
-  System.out.println(a);
   for(int i=1;i<=Working_Days_Per_Month;i++)
   {
+   int a = r.nextInt(3);
    switch(a) 
    {
     case 0 : System.out.println("Employee Present Full time");
@@ -26,6 +25,7 @@ public class empwage
     case 2 : System.out.println("Employee Absent");
              Total_Wage = 0;
              break;
+   default : System.out.println("Invalid");
    }
    Monthly_Wage += Total_Wage;
   }
