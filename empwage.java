25dc360f01d5e4bf3a6936231d8789874cd517f6
akilds <akilrainadesigan@gmail.com>
2021-03-24 20:59:32 +0530

    Class Method

--- empwage.java
+++ empwage.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class empwage
 {
- public static void main(String[] args)
+ public static int Total_Fulltime_Hrs = 8;
+ public static int Total_Parttime_Hrs = 4;
+ public static int Wage_Per_Hr = 20;
+ public static int emp_wage_calc()
  {
-  int Total_Fulltime_Hrs = 8;
-  int Total_Parttime_Hrs = 4;
-  int Wage_Per_Hr = 20;
   int Total_Wage = 0 ;
   int Monthly_Wage = 0;
   int Total_Hrs = 0;
@@ -15,26 +15,32 @@ public class empwage
   while(Total_Days<20 || Total_Hrs<100)
   {
    int a = r.nextInt(3);
-   switch(a) 
+   switch(a)
    {
     case 0 : System.out.println("Employee Present Full time");
              Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
              Total_Hrs = Total_Hrs + Total_Fulltime_Hrs;
              Total_Days++;
-				 break;
+             break;
     case 1 : System.out.println("Employee Present Part time");
              Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
              Total_Hrs += Total_Fulltime_Hrs;
-				 Total_Days++;
-				 break;
+             Total_Days++;
+             break;
     case 2 : System.out.println("Employee Absent");
              Total_Wage = 0;
              break;
     default : System.out.println("Invalid");
    }
-   Monthly_Wage += Total_Wage;
+     Monthly_Wage += Total_Wage;
   }
-  System.out.println("Mothly Wage : " + Monthly_Wage);
+  return Monthly_Wage;
+ }
+ public static void main(String[] args)
+ {
+  int Emp_Monthly_Wage;
+  Emp_Monthly_Wage = emp_wage_calc();
+  System.out.println("Mothly Wage : " + Emp_Monthly_Wage);
  }
 }
 
