ed0fb09bfe8795f33dec68f521f1150ba824c31f
akilds <akilrainadesigan@gmail.com>
2021-03-27 17:31:36 +0530

    Queried Display

--- empwage.java
+++ empwage.java
@@ -38,20 +38,17 @@ public class empwage
    int a = r.nextInt(3);
    switch(a)
    {
-    case 0 : System.out.println("Employee Present Full time");
-             Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
+    case 0 : Total_Wage = Total_Fulltime_Hrs * Wage_Per_Hr;
              Daily_Wage.add(Total_Wage);
              Total_Hrs = Total_Hrs + Total_Fulltime_Hrs;
              Total_Days++;
              break;
-    case 1 : System.out.println("Employee Present Part time");
-             Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
+    case 1 : Total_Wage = Total_Parttime_Hrs * Wage_Per_Hr;
              Daily_Wage.add(Total_Wage);
              Total_Hrs += Total_Fulltime_Hrs;
              Total_Days++;
              break;
-    case 2 : System.out.println("Employee Absent");
-             Total_Wage = 0;
+    case 2 : Total_Wage = 0;
              Daily_Wage.add(Total_Wage);
              break;
     default : System.out.println("Invalid");
@@ -61,24 +58,44 @@ public class empwage
   Dailywage_copy = Daily_Wage;
   return Monthly_Wage;
  }
- public static void Emp_Wage_Builder()
+ public static void Emp_Wage_Builder(char c)
  {
   ArrayList<CompanyEmpWage> emwage = new ArrayList<CompanyEmpWage>();
   ArrayList<Integer> wage_show = new ArrayList<Integer>();
   emwage.add(new CompanyEmpWage(Emp_Monthly_Wage,Dailywage_copy));
   Wage_Each_Company = emwage.get(0).EmpWageShow();
   wage_show = emwage.get(0).WageShow();
-  System.out.println("Mothly Wage of company : " + Wage_Each_Company);
-  System.out.println("Daily Wage : \n" + wage_show);
+  if(c=='Y'||c=='y')
+  {
+   System.out.println("Mothly Wage of company : " + Wage_Each_Company);
+   System.out.println("Daily Wage : \n" + wage_show);
+  }
  }
  public static void main(String[] args)
  {
+  Scanner sc = new Scanner(System.in);
+  char choice;
   Emp_Monthly_Wage = emp_wage_calc(100,20,20);
-  Emp_Wage_Builder();
+  System.out.println("Do you want to display (Y/N)");
+  choice = sc.next().charAt(0);
+  if(choice=='Y'||choice=='y')
+  {
+    Emp_Wage_Builder(choice);
+  }
   Emp_Monthly_Wage = emp_wage_calc(110,21,25);
-  Emp_Wage_Builder();
+  System.out.println("Do you want to display (Y/N)");
+  choice = sc.next().charAt(0);
+  if(choice=='Y'||choice=='y')
+  {
+    Emp_Wage_Builder(choice);
+  }
   Emp_Monthly_Wage = emp_wage_calc(120,22,30);
-  Emp_Wage_Builder();
+  System.out.println("Do you want to display (Y/N)");
+  choice = sc.next().charAt(0);
+  if(choice=='Y'||choice=='y')
+  {
+    Emp_Wage_Builder(choice);
+  }
  }
 }
 
